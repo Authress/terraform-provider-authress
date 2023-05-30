ae502701acf6ee0d94cfe65fb58d5adfc5f7758b
Warren Parad <gpg@warrenparad.net>
2023-05-30 11:21:26 +0200

    Add comments for how to improve version injection.

--- src/version.go
+++ src/version.go
@@ -6,6 +6,7 @@ type BuildInfo struct {
 
 func GetBuildInfo() BuildInfo {
     return BuildInfo{
+        // This should actually be set using: https://www.digitalocean.com/community/tutorials/using-ldflags-to-set-version-information-for-go-applications
         Version: "0.0.0",
     }
 }
